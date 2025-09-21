e3d3c6a6ca8e6bbd5996e40b82a3657d4154f14e
meronkahsay <kahsaymeront@gmail.com>
2025-09-21 15:02:08 +0300

    Added password-reset page

--- tesfa/src/app/api/password-reset-confirm/route.ts
+++ tesfa/src/app/api/password-reset-confirm/route.ts
@@ -1,23 +1,3 @@
-// import { NextResponse } from "next/server";
-// export async function POST(req: Request) {
-//   const body = await req.json();
-//   const backend = process.env.NEXT_PUBLIC_API_URL;
-//   const res = await fetch(`${backend}password-reset-confirm/`, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(body),
-//   });
-//   const text = await res.text();
-//   let data = text ? JSON.parse(text) : {};
-// try {
-//   data = text ? JSON.parse(text) : {};
-// } catch {
-//   data = { message: "Invalid response format from server." };
-// }
-//  return NextResponse.json(data, { status: res.status });
-// }
-
-
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
