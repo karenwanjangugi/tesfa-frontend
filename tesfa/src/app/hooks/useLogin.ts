9c44132f17d2bf09f4519cc24c0c47778a29697c
student <judygikuni@gmail.com>
2025-09-25 10:24:24 +0300

    feat: Added the admin login

--- tesfa/src/app/hooks/useLogin.ts
+++ tesfa/src/app/hooks/useLogin.ts
@@ -1,8 +1,9 @@
 
 'use client';
-
 import { useState } from 'react';
 import { fetchLogin } from '../utils/loginUtils';
+import { useRouter } from 'next/navigation';
+
 
 interface LoginCredentials {
   email: string;
@@ -11,24 +12,32 @@ interface LoginCredentials {
 
 interface LoginResponse {
   token: string; 
+  role: string;
      
 }
 
 const useLogin = () => {
+  const router = useRouter()
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
   const login = async (credentials: LoginCredentials): Promise<LoginResponse | null | undefined> => {
+
     setLoading(true);
     setError(null);
 
     try {
       const data = await fetchLogin(credentials);
-      
-      if (data?.token) {
-        return { token: data.token};
-      } 
-      
+      localStorage.setItem("authToken", data.token);
+      localStorage.setItem("userRole", data.role);
+
+      if(data.role === "admin"){
+        router.push("/admin/dashboard");
+
+      }else{
+        router.push("/dashboard");
+      }
+
      
     } catch (err) {
       setError((err as Error).message || 'Login failed');
